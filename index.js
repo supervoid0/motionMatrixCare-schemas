32ce37e87170260c6e0d6902feebc5344232f099
supervoid0 <sababnazmus@gmail.com>
2024-09-11 11:43:07 +0900

    add cloudFile schema

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   initMongo: require('./init'),
   User: require('./user/user.model'),
   Rehabilitant: require('./rehabilitant/rehabilitant.model'),
-  File: require('./file/file.model')
+  File: require('./file/file.model'),
+  CloudFile: require('./cloudFile/cloudFile.model')
 }
